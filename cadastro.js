c91addee5f0ebabc3efcc8c8cc501eee1abba1cf
ArielCSousa <arie.sousa@sptech.school>
2025-08-29 11:02:34 -0300

    Acrescentando entidades ao dbjson - nova tela exibindo aluno com CRUD para usuario editar-cadastrar-visualizar-deletar alunos  - listando alunos por usuario(cliente) logado

--- cadastro.js
+++ cadastro.js
@@ -1,29 +1,29 @@
 document.getElementById("formCadastro").addEventListener("submit", function(event) {
-  event.preventDefault(); // previne recarregar a página
+  event.preventDefault();
 
-  const nome = document.getElementById("nome").value;
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("senha").value;
+  const usuario = {
+    cnpj: document.getElementById("cnpj").value,
+    nome: document.getElementById("nome").value,
+    razaoSocial: document.getElementById("razaoSocial").value,
+    telefone: document.getElementById("telefone").value,
+    cep: document.getElementById("cep").value,
+    email: document.getElementById("email").value,
+    senha: document.getElementById("senha").value
+  };
 
   fetch("http://localhost:3000/usuarios", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ nome, email, senha })
+    body: JSON.stringify(usuario)
   })
   .then(res => res.json())
   .then(() => {
-    console.log("Cadastro concluído, redirecionando...");
-    // alert("Cadastro realizado com sucesso!");
-    window.location.href = "login.html"; // redireciona
+    window.location.href = "login.html";
+    alert("Cadastro realizado com sucesso!");
   })
-  .catch(err => {
-    console.error("Erro ao cadastrar:", err);
-    alert("Erro ao cadastrar. Veja o console.");
-  });
+  .catch(err => console.error("Erro ao cadastrar usuário:", err));
 });
 
-//muda para a tela de login pelo botão
-document.getElementById("btnLogin").addEventListener("click", function() {
-  // Volta para a página de login
+document.getElementById("btnLogin").addEventListener("click",()=>{
   window.location.href = "login.html";
-});
+})
